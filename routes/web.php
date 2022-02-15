ae57ece5ee51f325c9f75df1e05c4e58f48a4732
Thiago de Paula <thiago@tenil.com.br>
2022-02-15 18:28:40 -0300

    create controller and edit web route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AngularController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
-});
+});*/
+
+Route::any('/{any}', [AngularController::class, "AngularController@index"])->where('any', '^(?!api).*$');
